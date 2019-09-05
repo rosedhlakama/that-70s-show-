2dc4cf812f9b1cdd8938347ac9241fd99f4317a4
Student <student@pop-os.localdomain>
2019-09-06 10:30:45 +1200

    have angela

--- server/db.js
+++ server/db.js
@@ -1,9 +1,16 @@
 module.exports = {
-  getFruits
+  getFruits,
+  getImage
 }
 
 let fruits = ['banana', 'apple', 'feijoa']
 
 function getFruits () {
   return Promise.resolve(fruits)
+}
+
+let image = '/f92793b692b45c6881ef43caafb1c7c4.jpg '
+
+function getImage(){
+  return Promise.resolve(image)
 }
